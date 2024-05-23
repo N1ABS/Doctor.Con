620b92ec0abfda49e9f0210c57c8acefdcfbb9c2
Nurbolat Absagat <n.absagat@gmail.com>
2024-05-23 15:57:46 +0500

    Добавление функционала для клиник

---
+++ Doctor.Con/Controllers/ClinicsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Doctor.Con.Models;
+
+namespace Doctor.Con.Controllers
+{
+    public class ClinicsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Clinics
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Clinics.ToListAsync());
+        }
+
+        // GET: Clinics/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clinic clinic = await db.Clinics.FindAsync(id);
+            if (clinic == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clinic);
+        }
+
+        // GET: Clinics/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clinics/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,Rating")] Clinic clinic)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clinics.Add(clinic);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(clinic);
+        }
+
+        // GET: Clinics/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clinic clinic = await db.Clinics.FindAsync(id);
+            if (clinic == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clinic);
+        }
+
+        // POST: Clinics/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Rating")] Clinic clinic)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(clinic).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(clinic);
+        }
+
+        // GET: Clinics/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clinic clinic = await db.Clinics.FindAsync(id);
+            if (clinic == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clinic);
+        }
+
+        // POST: Clinics/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Clinic clinic = await db.Clinics.FindAsync(id);
+            db.Clinics.Remove(clinic);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
